7e1ce52a52ce595ae5d622a9fb84c2749147b22c
Jeremy Stone <74574922+jstone-uw@users.noreply.github.com>
2023-04-04 14:18:50 -0700

    API URL for production

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 export default {
+  // Production
+  apiBaseUrl: 'https://api.mavedb.org/api/v1'
+
+  // Local development
   // apiBaseUrl:  'http://localhost:8002/api/v1'
-  // apiBaseUrl: 'https://www.mavedb.org/api'
-  apiBaseUrl: 'https://dev.api.mavedb.lims.bbi.gs.washington.edu/api/v1'
+
+  // AWS test environment
+  // apiBaseUrl: 'https://dev.api.mavedb.lims.bbi.gs.washington.edu/api/v1'
 }

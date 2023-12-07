ad46ae454adbc1d13555e3fdf1635ecac4cd444b
ashsny <1389678+ashsny@users.noreply.github.com>
2023-12-07 13:03:10 -0800

    Set up dev scripts for Vite and update README.

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,16 @@ export default defineConfig({
     },
     extensions: ['.vue', '.tsx', '.ts', '.mjs', '.js', '.jsx', '.json', '.wasm'],
   },
-  preview: {
+  server: {
     port: 8081,
-    https: true,
-  }
+    // Our ORCID app only has a limited set of legal redirect URLs (which 
+    // includes localhost:8081 but not all other ports), so fail if the desired
+    // port is occupied.
+    strictPort: true, 
+  },
+  preview: {
+    port: 8082,
+    // Same as above, but localhost:8082 is also legal per ORCID.
+    strictPort: true,
+  },
 })

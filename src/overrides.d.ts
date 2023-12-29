38f715c1694b64baf75d78e11f45ec174a14cde2
ashsny <1389678+ashsny@users.noreply.github.com>
2023-12-29 10:19:19 -0800

    Remove seemingly unnecessary overrides and augments.

--- src/overrides.d.ts
+++ src/overrides.d.ts
@@ -1,9 +1,3 @@
 declare module "vue-debounce" {
     function debounce(fn: function(any): any, interval: string | number)
 }
-
-declare module "*.vue" {
-  import { defineComponent } from "vue";
-  const Component: ReturnType<typeof defineComponent>;
-  export default Component;
-}

0e3d833bbde948fd306df9f1fb98c5c945de8d49
Ben Capodanno <capodb@uw.edu>
2024-04-16 09:24:15 -0700

    Add Conditional to Experiment Page of TargetSequence Taxonomy

--- src/components/screens/ExperimentView.vue
+++ src/components/screens/ExperimentView.vue
@@ -109,8 +109,8 @@
                 {{ targetGene.targetAccession.accession }}
               </div>
 
-              <div v-if="targetGene.targetSequence.taxonomy?.url"><strong>Taxonomy ID:</strong> 
-                &nbsp;<a :href="`${targetGene.targetSequence.taxonomy.url}`" target="blank">{{targetGene.targetSequence.taxonomy.taxId}}</a> 
+              <div v-if="targetGene.targetSequence?.taxonomy?.url"><strong>Taxonomy ID:</strong>
+                &nbsp;<a :href="`${targetGene.targetSequence.taxonomy.url}`" target="blank">{{targetGene.targetSequence.taxonomy.taxId}}</a>
                 </div>
               <div v-if="targetGene.targetSequence?.sequence" style="word-break: break-word">
                 <div v-if="targetGene.targetSequence.taxonomy?.organismName"><strong>Organism name:</strong>

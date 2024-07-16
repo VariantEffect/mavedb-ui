6f049ce1d4bf0d117f2ae961d3287949ddf96ea7
ashsny <1389678+ashsny@users.noreply.github.com>
2024-07-15 21:09:05 -0700

    Add custom view, fix a few bugs.

--- src/lib/clinvar.ts
+++ src/lib/clinvar.ts
@@ -1,17 +1,41 @@
 export const CLNSIG_DISPLAY_NAMES = {
-    'Likely_pathogenic': 'Likely pathogenic variant',
-    'Pathogenic': 'Pathogenic variant',
-    'Pathogenic/Likely_pathogenic': 'Pathogenic/Likely pathogenic variant (in different submissions)',
-    'Likely_benign': 'Likely benign variant',
-    'Benign': 'Benign variant',
-    'Benign/Likely_benign': 'Benign/Likely benign variant (in different submissions)',
-    'Uncertain_significance': 'Variant of uncertain significance',
-    'Conflicting_interpretations_of_pathogenicity': 'Variant with conflicting interpretations of pathogenicity',
+    'Pathogenic': {
+        variant_name: 'Pathogenic variant',
+        short_name: 'Pathogenic',
+    },
+    'Likely_pathogenic': {
+        variant_name: 'Likely pathogenic variant',
+        short_name: 'LP',
+    },
+    'Pathogenic/Likely_pathogenic': {
+        variant_name: 'Pathogenic/Likely pathogenic variant (in different submissions)',
+        short_name: 'Path/LP (both)',
+    },
+    'Benign': {
+        variant_name: 'Benign variant',
+        short_name: 'Benign',
+    },
+    'Likely_benign': {
+        variant_name: 'Likely benign variant',
+        short_name: 'LB',
+    },
+    'Benign/Likely_benign': {
+        variant_name: 'Benign/Likely benign variant (in different submissions)',
+        short_name: 'B/LB (both)',
+    },
+    'Uncertain_significance': {
+        variant_name: 'Variant of uncertain significance',
+        short_name: 'VUS',
+    },
+    'Conflicting_interpretations_of_pathogenicity': {
+        variant_name: 'Variant with conflicting interpretations of pathogenicity',
+        short_name: 'Conflicting',
+    },
 }
 
 export const CLNREVSTAT_STARS = {
     'no_assertion_criteria_provided': 0,
-    'criteria_provided,_conflicting_interpretaions': 1,
+    'criteria_provided,_conflicting_interpretations': 1,
     'criteria_provided,_single_submitter': 1,
     'criteria_provided,_multiple_submitters,_no_conflicts': 2,
     'reviewed_by_expert_panel': 3,

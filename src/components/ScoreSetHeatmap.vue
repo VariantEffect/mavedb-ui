dbb13ac3b891989d9355e628481275d02143f29e
Ben Capodanno <capodb@uw.edu>
2024-11-18 14:16:32 -0800

    Always Display Heatmap Scrollbar on OSX
    WebKit default behavior on OSX 10.7+ is to hide the scrollbar when not in use. This obscures the fact that the heatmap is a scrollable component.

--- src/components/ScoreSetHeatmap.vue
+++ src/components/ScoreSetHeatmap.vue
@@ -439,4 +439,15 @@ export default {
   visibility: hidden;
 }
 
+::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 7px;
+  height: .5em;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, .5);
+  box-shadow: 0 0 1px rgba(255, 255, 255, .5);
+}
 </style>

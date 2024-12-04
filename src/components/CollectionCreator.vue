ce2e7539d82d26238490850580c1a05721093a2d
Sally Grindstaff <sallybg@uw.edu>
2024-12-04 10:42:11 -0800

    Create collection from collection adder dialog

--- src/components/CollectionCreator.vue
+++ src/components/CollectionCreator.vue
@@ -21,12 +21,7 @@
 
     <!-- TODO if getting here from the collection adder, should not show option to add score sets and experiments,
     but should show that option if getting here another way. probably handle this with a prop
-
-
-    add people to collection by their orcid id:
-        viewers (Chips)
-        editors (Chips)
-        admins (Chips) -->
+    -->
     <div class="contributors-adder">
         <div class="contributors-adder-title">
             Add contributors
@@ -35,26 +30,64 @@
             <label for="input-viewers">Viewers</label>
             <Chips
                 ref="viewersInput"
-                v-model="viewers"
+                v-model="contributors.viewers"
                 id="input-viewers"
                 :addOnBlur="true"
                 :allowDuplicate="false"
                 placeholder="Type or paste ORCID IDs here."
-                @add="newViewersAdded"
-                @keyup.escape="clearViewerSearch"
+                @add="newContributorsAdded('viewers', $event)"
+                @keyup.escape="clearContributorSearch('viewers')"
             >
                 <template #chip="slotProps">
                 <div>
-                    <div v-if="slotProps.value.givenName || slotProps.value.familyName">{{ slotProps.value.givenName }} {{ slotProps.value.familyName }} ({{ slotProps.value.orcidId }})</div>
+                    <div v-if="slotProps.value.firstName || slotProps.value.lastName">{{ slotProps.value.firstName }} {{ slotProps.value.lastName }} ({{ slotProps.value.orcidId }})</div>
                     <div v-else>{{ slotProps.value.orcidId }}</div>
                 </div>
                 </template>
             </Chips>
-            <!-- TODO add validation error logging here -->
+            <label for="input-editors">Editors</label>
+            <Chips
+                ref="editorsInput"
+                v-model="contributors.editors"
+                id="input-editors"
+                :addOnBlur="true"
+                :allowDuplicate="false"
+                placeholder="Type or paste ORCID IDs here."
+                @add="newContributorsAdded('editors', $event)"
+                @keyup.escape="clearContributorSearch('editors')"
+            >
+                <template #chip="slotProps">
+                <div>
+                    <div v-if="slotProps.value.firstName || slotProps.value.lastName">{{ slotProps.value.firstName }} {{ slotProps.value.lastName }} ({{ slotProps.value.orcidId }})</div>
+                    <div v-else>{{ slotProps.value.orcidId }}</div>
+                </div>
+                </template>
+            </Chips>
+            <label for="input-admins">Admins</label>
+            <Chips
+                ref="adminsInput"
+                v-model="contributors.admins"
+                id="input-admins"
+                :addOnBlur="true"
+                :allowDuplicate="false"
+                placeholder="Type or paste ORCID IDs here."
+                @add="newContributorsAdded('admins', $event)"
+                @keyup.escape="clearContributorSearch('admins')"
+            >
+                <template #chip="slotProps">
+                <div>
+                    <div v-if="slotProps.value.firstName || slotProps.value.lastName">{{ slotProps.value.firstName }} {{ slotProps.value.lastName }} ({{ slotProps.value.orcidId }})</div>
+                    <div v-else>{{ slotProps.value.orcidId }}</div>
+                </div>
+                </template>
+            </Chips>
         </div>
-
     </div>
 
+    <div>
+        <Button label="Cancel" severity="secondary" @click="cancel" />
+        <Button label="Save" @click="saveCollection" />
+    </div>
 
 </div>
 
@@ -64,6 +97,7 @@
 
 import axios from 'axios'
 import _ from 'lodash'
+import Button from 'primevue/button'
 import Chips from 'primevue/chips'
 import InputSwitch from 'primevue/inputswitch'
 import InputText from 'primevue/inputtext'
@@ -74,93 +108,148 @@ import {ORCID_ID_REGEX} from '@/lib/orcid'
 export default {
     name: 'CollectionCreator',
 
-    components: { Chips, InputSwitch, InputText },
+    emits: ['createdCollection', 'canceled'],
+
+    components: { Button, Chips, InputSwitch, InputText },
 
     data: () => ({
         collectionName: null,
         collectionDescription: null,
         collectionPrivate: true,
-        viewers: [],
-        editors: [],
-        admins: []
+        contributors: {
+            viewers: [],
+            editors: [],
+            admins: []
+        }
     }),
 
     methods: {
-
-        clearViewerSearch: function() {
+        clearContributorSearch: function(contributorType) {
             // This could change with a new PrimeVue version.
-            const input = this.$refs.viewersInput
+            const input = this.$refs[`${contributorType}Input`]
             input.$refs.input.value = ''
         },
 
         lookupUser: async function(orcidId) {
             // look up MaveDB user by Orcid ID
-            let allUsers = null
             let user = null
             try {
-                allUsers = (await axios.get(`${config.apiBaseUrl}/users`)).data
-                user = allUsers.filter((u) => u.username == orcidId)
-                // TODO is this too inefficient?
-                // should we load all users ahead of time so we don't have to do this call every time a new user is added?
-
+                user = (await axios.get(`${config.apiBaseUrl}/users/${orcidId}`)).data
             } catch (err) {
                 // Assume that the error was 404 Not Found.
             }
 
-            if (user) {
-                return orcidUser
-            } else {
-                // TODO throw error
-            }
+            return user
         },
 
-        //TODO when validating orcid id, do not allow duplicates between viewers, editors, and admins
+        newContributorsAdded: async function(contributorType, event) {
+            // TODO enforce that contributorType can only be "viewers", "editors", or "admins"
+            const newContributors = event.value
 
-        newViewersAdded: async function(event) {
-            const newViewers = event.value
-
-            // Convert any strings to ORCID users without names.
-            this.viewers = this.viewers.map((c) => _.isString(c) ? {orcidId: c} : c)
+            // Convert any strings to ORCID users without names,
+            // and create all contributors list, which will be checked against for duplicates
+            const contributorTypes = ["viewers", "editors", "admins"]
+            let allContributors = []
+            for (const contributorTypeTemp of contributorTypes) {
+                this.contributors[contributorTypeTemp] = this.contributors[contributorTypeTemp].map((c) => _.isString(c) ? {orcidId: c} : c)
+                allContributors = allContributors.concat(this.contributors[contributorTypeTemp])
+            }
 
             // Validate and look up each new contributor.
-            for (const newViewer of newViewers) {
-                if (_.isString(newViewer)) {
-                    const orcidId = newViewer.trim()
-                    if (orcidId && this.viewers.filter((v) => v.orcidId == orcidId).length > 1) {
-                        const firstIndex = _.findIndex(this.viewers, (c) => c.orcidId == orcidId)
-                        _.remove(this.viewers, (v, i) => i > firstIndex && v.orcidId == orcidId)
-                    } else if (orcidId && ORCID_ID_REGEX.test(orcidId)) {
-                        // Look up the ORCID ID in MaveDB (only allow for existing MaveDB users to be listed as contributors).
-                        //const orcidUser = await this.lookupOrcidUser(orcidId)
-                        const user = await this.lookupUser(orcidId)
-
-                        if (user) {
-                        // If found, update matching viewers. (There should only be one.)
-                        for (const viewer of this.viewers) {
-                            if (viewer.orcidId == user.orcidId) {
-                            _.merge(viewer, user)
-                            }
-                        }
-                        } else {
-                        // Otherwise remove the ciewer.
-                        _.remove(this.viewers, (c) => c.orcidId == orcidId)
+            for (const newContributor of newContributors) {
+                if (_.isString(newContributor)) {
+                    const orcidId = newContributor.trim()
+                    // check for duplicates in contributorType, and then in entire list
+                    // need to check in contributorType separately because if there is a duplicate within
+                    // that list, then we need the index
+                    // use else if, because there are already no duplicates between lists.
+                    // so if there is a match in one list, there will be no matches in the other lists
+                    if (orcidId && this.contributors[contributorType].filter((c) => c.orcidId == orcidId).length > 1) {
+                        const firstIndex = _.findIndex(this.contributors[contributorType], (c) => c.orcidId == orcidId)
+                        _.remove(this.contributors[contributorType], (c, i) => i > firstIndex && c.orcidId == orcidId)
+                    } else if (orcidId && allContributors.filter((c) => c.orcidId == orcidId).length > 1) {
+                        // if there is a match with another contributor type, remove the contributor
+                        // from the list it was just added to (this.$contributorType)
+                        _.remove(this.contributors[contributorType], (c) => c.orcidId == orcidId)
                         this.$toast.add({
                             life: 3000,
                             severity: 'warn',
-                            summary: `No MaveDB user was found with ORCID ID ${orcidId}.`
+                            summary: "Each contributor can only be assigned to one role per collection."
                         })
+                    } else if (orcidId && ORCID_ID_REGEX.test(orcidId)) {
+                        // Look up the ORCID ID in MaveDB (only allow for existing MaveDB users to be listed as contributors).
+                        const user = await this.lookupUser(orcidId)
+
+                        if (user) {
+                            // If found, update matching contributors. (There should only be one.)
+                            for (const contributor of this.contributors[contributorType]) {
+                                if (contributor.orcidId == user.orcidId) {
+                                _.merge(contributor, user)
+                                }
+                            }
+                        } else {
+                            // Otherwise remove the contributor.
+                            _.remove(this.contributors[contributorType], (c) => c.orcidId == orcidId)
+                            this.$toast.add({
+                                life: 3000,
+                                severity: 'warn',
+                                summary: `No MaveDB user was found with ORCID ID ${orcidId}.`
+                            })
                         }
                     } else {
-                        _.remove(this.contributors, (c) => c.orcidId == orcidId)
+                        _.remove(this.contributors[contributorType], (c) => c.orcidId == orcidId)
                         this.$toast.add({
-                        life: 3000,
-                        severity: 'warn',
-                        summary: `${orcidId} is not a valid ORCID ID`
+                            life: 3000,
+                            severity: 'warn',
+                            summary: `${orcidId} is not a valid ORCID ID`
                         })
                     }
                 }
             }
+
         },
+
+        saveCollection: async function() {
+            // check for required fields
+            if (this.collectionName != null) {
+                // create object that has field names assigned to this.<field name>
+                const newCollection = {
+                    name: this.collectionName,
+                    description: this.collectionDescription,
+                    private: this.collectionPrivate,
+                    viewers: this.contributors.viewers, 
+                    editors: this.contributors.editors,
+                    admins: this.contributors.admins
+                }
+                // then, try to call the api with the object as body and catch errors - if error, send toast message with severity = error
+                let response = null
+                try {
+                    response = await axios.post(`${config.apiBaseUrl}/collections`, newCollection)
+                } catch (e) {
+                    response = e.response || { status: 500 }
+                    this.$toast.add({ severity: 'error', summary: 'Error', life: 3000 })
+                }
+                // if there is an error, probably keep the dialog open. log errors to console?
+                // then, if status is 200, throw toast success message and emit the saved collection
+                if (response.status == 200) {
+                    const savedCollection = response.data
+                    this.$toast.add({ severity: 'success', summary: 'Created new collection.', life: 3000 })
+                    this.$emit('createdCollection', savedCollection)
+                } else {
+                    // TODO log errors to console here
+                }
+            } else {
+                this.$toast.add({
+                    life: 3000,
+                    severity: 'warn',
+                    summary: "Must provide collection name"
+                })                
+            }
+        },
+
+        cancel: function() {
+            this.$emit('canceled')
+        }
 
     }
 }

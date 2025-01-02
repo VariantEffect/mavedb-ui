b24dd7540f223c4e23676ecca6e01cb2489699d7
Ben Capodanno <capodb@uw.edu>
2025-01-02 11:20:51 -0800

    Emit Calibration Selection Changes

--- src/components/CalibrationTable.vue
+++ src/components/CalibrationTable.vue
@@ -1,6 +1,11 @@
 <template>
     <table v-if="evidenceStrengthsForDisplay" class="mave-calibrations-table">
         <tr>
+            <td colspan="100" class="dropdown" >
+                <Dropdown v-model="activeCalibrationKey" :options="Object.keys(calibrations)" :optionLabel="titleCase" style="width: 100%;" :disabled="Object.keys(calibrations).length < 2" />
+            </td>
+        </tr>
+        <tr>
             <th>Evidence Strength</th>
             <th v-for="evidenceStrength of evidenceStrengthsForDisplay" :key="evidenceStrength"><b>{{ evidenceStrength }}</b></th>
         </tr>
@@ -18,26 +23,33 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
 import { Calibrations } from '@/lib/calibrations';
+import Dropdown from 'primevue/dropdown';
 
 export default defineComponent({
     name: 'CalibrationTable',
-    components: { },
+    components: { Dropdown },
+
+    data: function() {
+        const defaultCalibration = Object.keys(this.calibrations)[0]
+        this.$emit('calibrationSelected', defaultCalibration)
+
+        return {
+            activeCalibrationKey: defaultCalibration
+        }
+    },
+
+    emits: ['calibrationSelected'],
 
     props: {
         calibrations: {
             type: Object as () => {[key: string]: Calibrations},
             required: true
         },
-
-        selectedCalibration: {
-            type: String,
-            required: true
-        }
     },
 
     computed: {
         selectedCalibrations: function () {
-            return this.calibrations[this.selectedCalibration]
+            return this.calibrations[this.activeCalibrationKey]
         },
 
         evidenceStrengthsForDisplay: function() {
@@ -58,6 +70,14 @@ export default defineComponent({
             return s.replace (/^[-_]*(.)/, (_, c) => c.toUpperCase())       // Initial char (after -/_)
                     .replace (/[-_]+(.)/g, (_, c) => ' ' + c.toUpperCase()) // First char after each -/_
         }
+    },
+
+    watch: {
+        activeCalibrationKey: {
+            handler: function() {
+                this.$emit('calibrationSelected', this.activeCalibrationKey)
+            }
+        }
     }
 })
 </script>
@@ -66,6 +86,12 @@ export default defineComponent({
 table.mave-calibrations-table {
    border-collapse: collapse;
    margin: 1em auto 0.5em auto;
+}
+
+table.mave-calibrations-table .dropdown {
+    border-top-style: hidden;
+    border-left-style: hidden;
+    border-right-style: hidden;
 }
 
 table.mave-calibrations-table td,

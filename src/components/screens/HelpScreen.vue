5a81bd0c32117bdad8c4db0623a32ef754156475
Jeremy Stone <74574922+jstone-uw@users.noreply.github.com>
2025-09-22 21:17:43 -0700

    Zulip link update and code formatting

--- src/components/screens/HelpScreen.vue
+++ src/components/screens/HelpScreen.vue
@@ -9,45 +9,47 @@
       <ul>
         <li>
           <strong>Chat with us:</strong>
-          <a href="https://zulip.mavedb.com" target="_blank" rel="noopener"> Join our Zulip chat</a> to ask questions,
-          get help, or discuss upcoming development work.
+          <a href="https://mavedb.zulipchat.com" rel="noopener" target="_blank"> Join our Zulip chat</a> to ask
+          questions, get help, or discuss upcoming development work.
         </li>
         <li>
           <strong>Documentation:</strong>
-          <a href="/docs" target="_blank" rel="noopener"> Check out our documentation</a> for guides and reference.
+          <a href="/docs" rel="noopener" target="_blank"> Check out our documentation</a> for guides and reference.
         </li>
         <li>
           <strong>Submit a bug report or feature request:</strong>
-          You can use our <a href="https://zulip.mavedb.com" target="_blank" rel="noopener">Zulip chat</a> to let us
+          You can use our <a href="https://mavedb.zulipchat.com" rel="noopener" target="_blank">Zulip chat</a> to let us
           know about any
           <a
             href="https://mavedb.zulipchat.com/#narrow/channel/511174-bug-reports/topic/channel.20events/with/525122858"
-            target="_blank"
             rel="noopener"
+            target="_blank"
             >bugs</a
           >
           causing you problems or any
           <a
             href="https://mavedb.zulipchat.com/#narrow/channel/511173-features/topic/channel.20events/with/525122848"
-            target="_blank"
             rel="noopener"
+            target="_blank"
             >features</a
           >
           you'd like to see. Alternatively, you can submit an issue directly on our
-          <a href="github.com/varianteffect/mavedb-ui/issues" target="_blank" rel="noopener">GitHub repository</a>.
+          <a href="github.com/varianteffect/mavedb-ui/issues" rel="noopener" target="_blank">GitHub repository</a>.
         </li>
       </ul>
     </div>
   </DefaultLayout>
 </template>
 
-<script>
+<script lang="ts">
+import {defineComponent} from 'vue'
+
 import DefaultLayout from '@/components/layout/DefaultLayout.vue'
 
-export default {
+export default defineComponent({
   name: 'HelpScreen',
   components: {DefaultLayout}
-}
+})
 </script>
 
 <style scoped>

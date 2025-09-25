3c40e497461c0eb845fe5cf32957d42a6bb0916d
David Reinhart <davidrr@uw.edu>
2025-09-25 16:15:37 -0700

    Fix logic for handling validation errors in ExperimentCreator component save function.

--- src/components/screens/ExperimentCreator.vue
+++ src/components/screens/ExperimentCreator.vue
@@ -1235,6 +1235,7 @@ export default {
           this.$toast.add({severity: 'success', summary: 'The new experiment was saved.', life: 3000})
         }
       } else if (response.data && response.data.detail) {
+        this.serverSideValidationErrors = {}
         if (typeof response.data.detail === 'string' || response.data.detail instanceof String) {
           // Handle generic errors that are not surfaced by the API as objects
           this.$toast.add({
@@ -1252,9 +1253,7 @@ export default {
             formValidationErrors[path] = error.msg
           }
           this.serverSideValidationErrors = formValidationErrors
-          this.mergeValidationErrors()
         }
-        this.serverSideValidationErrors = formValidationErrors
         this.mergeValidationErrors()
         this.activeWizardStep = this.minStepWithError()
       }

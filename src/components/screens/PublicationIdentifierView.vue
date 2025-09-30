782095da6c2ca2cfef93627408df13f430de74c9
Jeremy Stone <74574922+jstone-uw@users.noreply.github.com>
2025-09-30 09:14:38 -0700

    Obsolete comment

--- src/components/screens/PublicationIdentifierView.vue
+++ src/components/screens/PublicationIdentifierView.vue
@@ -131,7 +131,6 @@ export default {
           }
         )
         // TODO (#130) catch errors in response
-        // TODO Handle the case where there were more matching score sets than the search result limit.
         this.scoreSets = response.data?.scoreSets || []
 
         // reset published score sets search results when using search bar

067ed6e995945391233ae0ad685635278408a27d
Benjamin Capodanno <bencap@outlook.com>
2026-03-05 16:10:46 -0800

    chore: remove unused Storybook setup
    Storybook was configured (main.ts, preview.ts, deps, scripts) but had
    zero story files. Remove all config, dependencies, and related ESLint
    and gitignore entries.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import pluginVue from 'eslint-plugin-vue'
 import {defineConfigWithVueTs, vueTsConfigs} from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
